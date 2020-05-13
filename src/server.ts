fde61ef1b9bd1228d6e1f92efe9e3f4222f4e7e0
Cristian Araujo <cristian.jesus@zup.com.br>
2020-05-12 22:15:48 -0300

    Configurando Estrutura

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { request, response } from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello Cristian' });
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
