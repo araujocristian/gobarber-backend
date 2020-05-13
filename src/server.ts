ff721fc922c42a34cad4cc5db1399104f2561449
Cristian Araujo <cristian.jesus@zup.com.br>
2020-05-12 23:12:34 -0300

    Configurando EditorConfig, EsLint e TS Import

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Cristian' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
