c587e700395bfc45beba5c148520f7bd6351ed9b
Cristian Araujo <araujocristian@live.com>
2020-06-20 19:17:32 -0300

    Criando model de agendamento

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,18 +1,22 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
 
   // Função TS Omit<Tipo, Variavel para omitir>
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
+  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  //   this.id = uuid();
+  //   this.provider = provider;
+  //   this.date = date;
+  // }
 }
 
 export default Appointment;
