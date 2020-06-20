90cd11c06e3996467a703c46bea7e610130ec13e
Cristian Araujo <araujocristian@live.com>
2020-06-20 19:08:51 -0300

    Criando tabela de agendamentos

---
+++ src/database/migrations/1592669972271-CreateApointments.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateApointments1592669972271
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
+
+/**
+ * Migrations funcionam como uma linha do tempo da base da aplicação,
+ * mantendo a base versiondada.
+ *
+ * O função do metodo down é desfazer o que o que foi executado no metodo up
+ *
+ * Você só pode alterar uma migration se ela ainda não foi compartilhada, ex:
+ * GIT.
+ * Se não, será necessario criar uma nova migration com a alteração
+ */
