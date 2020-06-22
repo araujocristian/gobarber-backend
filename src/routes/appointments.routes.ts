66e10d6f38ea25ea03c040d5108a70a2e5d94ef2
Cristian Araujo <araujocristian@live.com>
2020-06-22 00:19:01 -0300

    Lidando com erros

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -23,23 +23,19 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    // Diferenciar transformação do dado de regra de negócio
-    const parsedDate = parseISO(date);
+  // Diferenciar transformação do dado de regra de negócio
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      provider_id,
-    });
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
 
-    return response.json({ appointment });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json({ appointment });
 });
 
 export default appointmentsRouter;
