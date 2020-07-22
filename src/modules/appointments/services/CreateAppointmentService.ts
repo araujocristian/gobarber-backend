12d3e694f56c9dafc891381d6369893d69a6a5dc
Cristian Araujo <araujocristian@live.com>
2020-07-21 23:12:15 -0300

    Criando Camada de Infra

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import AppError from '../errors/AppError';
 
-import Appointment from '../entities/Appointment';
+import Appointment from '../infra/typeorm/entities/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 /**
  * [x] Recebimento das informações
