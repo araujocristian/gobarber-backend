e01ee2dfe5143ac56cdddf5c305a3b20a89dc1e8
Cristian Araujo <araujocristian@live.com>
2020-08-10 15:33:10 -0300

    Salvando tokens no banco

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,9 +9,17 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
   AppointmentsRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
 
 container.registerSingleton<IUsersRepository>(
