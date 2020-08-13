5895530b93c833a620a1b141a2151bc3237832e8
Cristian Araujo <araujocristian@live.com>
2020-08-13 20:28:35 -0300

    Utilizando Class Transformer

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -5,7 +5,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
-
+import { Exclude, Expose } from 'class-transformer';
 // KISS - Keep It Simple & Stupid
 
 @Entity('users')
@@ -20,6 +20,7 @@ class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @Column()
@@ -30,6 +31,13 @@ class User {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: 'avatar_url' })
+  getAvatarUrl(): string | null {
+    return this.avatar
+      ? `${process.env.APP_API_URL}/files/${this.avatar}`
+      : null;
+  }
 }
 
 export default User;
