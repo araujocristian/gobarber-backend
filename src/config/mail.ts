f4858c124aefdd5e93d18c5e5944ef093b5d0673
Cristian Araujo <araujocristian@live.com>
2020-08-13 22:57:17 -0300

    configurando Emails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'cristian@gobaber.com.br',
+      name: 'Cristian Araujo',
+    },
+  },
+} as IMailConfig;
